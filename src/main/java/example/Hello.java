5de15aa6d39aa17bdf30ff81eda3f9234488cdfe
Bastian Bukatz <bassualdo@arcor.de>
2017-03-29 18:51:22 +0200

    implemented first gradle build of a java lambda fuction :)

---
+++ src/main/java/example/Hello.java
@@ -0,0 +1,9 @@
+package ch.bassinator.shareOkey.javaLampdaBlueprint.example;
+
+import com.amazonaws.services.lambda.runtime.Context; 
+
+public class Hello {
+    public String myHandler(int myCount, Context context) {
+        return String.valueOf(myCount);
+    }
+}
